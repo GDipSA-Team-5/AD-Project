a5a526d080deddd34ff9376f80ea0f8a0271d975
AT-WhemblyWhermer <e1674574@u.nus.edu>
2026-01-23 22:05:56 +0800

    added trial unit tests for CalculatorTests class

---
+++ ADWebApplication.Tests/CalculatorTests.cs
@@ -0,0 +1,15 @@
+﻿namespace ADWebApplication.Tests;
+
+using Xunit;
+using ADWebApplication.Services;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_TwoNumbers_ReturnsCorrectSum()
+    {
+        var calc = new Calculator();
+        int result = calc.Add(2, 3);
+        Assert.Equal(5, result);
+    }
+}
