fd937a7e9bf9692d5bcb3d0a3bc8f24b749dcff9
AT-WhemblyWhermer <e1674574@u.nus.edu>
2026-02-06 02:45:22 +0800

    debug sca

--- scripts/sca-dependency-check.sh
+++ scripts/sca-dependency-check.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-# Directory for cached data (NVD, CISA, RetireJS)
 DATA_DIR="./dependency-check-data"
 mkdir -p "$DATA_DIR"
 
-# Run Dependency-Check
-# - Automatically updates DB if missing
-# - Uses local cache for subsequent runs
-/usr/local/bin/dependency-check \
+dependency-check.sh \
   --project ADWebApplication \
   --scan . \
   --format ALL \
