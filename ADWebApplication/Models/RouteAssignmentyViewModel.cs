1f553ec28d75bacc6748ff7024ddc3491a5907fe
Angeline <angelineyzh@gmail.com>
2026-02-07 10:51:41 +0800

    tidied things

--- ADWebApplication/Models/RouteAssignmentyViewModel.cs
+++
@@ -1,9 +0,0 @@
-using ADWebApplication.Models;
-
-namespace ADWebApplication.Models;
-
-public class RouteAssignmentViewModel
-{
-    public string? OfficerUsername { get; set; }
-    public List<RouteAssignment>? RouteAssignments { get; set; }
-}
